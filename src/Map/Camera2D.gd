5ff72556d7c32785027d9c76239e786c4b71faf5
mbty <mbty@protonmail.com>
2023-04-30 23:52:37 +0200

    camera bounds first version

--- src/Map/Camera2D.gd
+++ src/Map/Camera2D.gd
@@ -21,6 +21,8 @@ var transition_status = null
 var transition_from = null
 var zoom_from = null
 
+@onready var bounds = get_node("../Bounds")
+
 func _ready():
 	default_position = global_position
 
@@ -73,7 +75,9 @@ func _physics_process(delta):
 			buffer += Vector2(0, -1)
 		if (Input.is_action_pressed("ui_down") && !Input.is_action_pressed("zoom-out")):
 			buffer += Vector2(0, 1)
-		global_position += delta*control_speed*buffer.normalized()
+		var predicted_pos = global_position + delta*control_speed*buffer.normalized()
+		if (bounds.get_node("CollisionShape2D").shape.get_rect().has_point(predicted_pos)):
+			global_position = predicted_pos
 		var zoom_diff = 0.0
 		if (Input.is_action_pressed("zoom-in")):
 			zoom_diff += 1
