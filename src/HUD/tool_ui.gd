8260bf26b8b2c35af640117344b5ae343813542e
Nicolas BUSSER <nicolas.busser67@gmail.com>
2023-04-30 12:54:27 +0200

    add printing of items

--- src/HUD/tool_ui.gd
+++ src/HUD/tool_ui.gd
@@ -4,9 +4,11 @@ signal tool_selected
 
 var tool_template
 @onready var tool_texture = $ToolTexture
+@onready var quantity = $Quantity
 
 # Called when the node enters the scene tree for the first time.
 func init(tool_template):
 	self.tool_template = tool_template
 	tool_texture.texture = tool_template.get_texture()
 	self.tooltip_text = tool_template.get_tool_name()
+	self.quantity.set_text(str(tool_template.get_quantity()))
