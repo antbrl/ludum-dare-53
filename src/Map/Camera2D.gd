5b07f6444837bd0f66b2ac5dc382439dce3f2bec
mbty <mbty@protonmail.com>
2023-04-30 19:17:11 +0200

    improve camera transitions

--- src/Map/Camera2D.gd
+++ src/Map/Camera2D.gd
@@ -3,7 +3,9 @@ extends Camera2D
 var followed = null
 var default_position = null
 
-const transition_duration = 1.0
+const transition_move_duration = 0.8
+const transition_zoom_duration = 0.5
+const total_transition_duration = 1.0
 const default_zoom = Vector2(1, 1)
 const follow_zoom = Vector2(2, 2)
 
@@ -29,14 +31,18 @@ func follow(object):
 func _physics_process(delta):
 	if transition_status != null:
 		transition_status += delta
-		var transition_to = default_position
-		if (followed != null):
-			transition_to = followed.global_position
-			zoom = Tween.interpolate_value(zoom_from, follow_zoom - zoom_from, transition_status, transition_duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-		else:
-			zoom = Tween.interpolate_value(zoom_from, default_zoom - zoom_from, transition_status, transition_duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-		global_position = Tween.interpolate_value(transition_from, transition_to - transition_from, transition_status, transition_duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-		if (transition_status > transition_duration):
+		if transition_status < transition_move_duration:
+			var transition_to = default_position
+			if (followed != null):
+				transition_to = followed.global_position
+			global_position = Tween.interpolate_value(transition_from, transition_to - transition_from, transition_status, transition_move_duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		if (transition_status > total_transition_duration - transition_zoom_duration):
+			var zoom_transition_status = transition_status - (total_transition_duration - transition_zoom_duration)
+			if (followed != null):
+				zoom = Tween.interpolate_value(zoom_from, follow_zoom - zoom_from, zoom_transition_status, transition_zoom_duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+			else:
+				zoom = Tween.interpolate_value(zoom_from, default_zoom - zoom_from, zoom_transition_status, transition_zoom_duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		if (transition_status > total_transition_duration):
 			transition_status = null
 	elif followed != null:
 		global_position = followed.global_position
