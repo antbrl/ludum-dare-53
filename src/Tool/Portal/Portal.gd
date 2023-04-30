5bb5ce75d199997854c05c8862df8e406bf84f21
Louis MOUHAT <louis.mouhat@gmail.com>
2023-05-01 01:10:02 +0200

    implement portals

--- src/Tool/Portal/Portal.gd
+++ src/Tool/Portal/Portal.gd
@@ -19,3 +19,19 @@ func _update_placement():
 			rotation_degrees = 270
 		_:
 			rotation_degrees = 0
+
+const VELOCITY_SQUARED_THRESHOLD = 50
+func add_physics_modifier(crate: Crate):
+	var dir_vector = Vector2.from_angle(PI - self.rotation)
+	if crate.linear_velocity.dot(dir_vector) < 0:
+		if crate.linear_velocity.project(dir_vector).length_squared() < VELOCITY_SQUARED_THRESHOLD:
+			crate.linear_velocity = Vector2.ZERO
+			return
+		var portals = get_tree().get_nodes_in_group('portals')
+		if portals.size() > 1:
+			var other_portal = portals.filter(func(p): return p != self)[0]
+			var angle_to = self.rotation + PI - other_portal.rotation
+			var new_velocity = crate.linear_velocity.rotated(angle_to)
+			crate.linear_velocity = new_velocity
+			crate.rotation += angle_to
+			crate.position = other_portal.position + Vector2.from_angle(PI - other_portal.rotation) * 5
