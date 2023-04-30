fcd1eb419a038668e3c70dcf1904f734939d8d83
ValerianC <valeriancoulon@gmail.com>
2023-04-30 19:47:52 +0200

    Better killzone for singularity

--- src/Singularity/singularity.gd
+++ src/Singularity/singularity.gd
@@ -2,21 +2,19 @@ extends PhysicsTool
 
 class_name Singularity
 
-const FAILURE_DISTANCE_SQ_THRESHOLD = 20
-const FAILURE_VELOCITY_SQ_THRESHOLD = 4
+const FAILURE_DISTANCE_SQ_THRESHOLD = 200
 
 const modif = 2e5
-const MAX_VELOCITY = Vector2(500, 500);
+const MAX_FORCE = Vector2(2e3, 2e3);
 
 
 func add_physics_modifier(crate: Crate):
 	var dir = self.position - crate.position
 	var distance_sq = dir.length_squared()
 	var force = modif * dir.normalized() / distance_sq
-	force = force.clamp(-MAX_VELOCITY, MAX_VELOCITY)
+	force = force.clamp(-MAX_FORCE, MAX_FORCE)
 	crate.apply_central_impulse(force)
 	
-	if distance_sq < FAILURE_DISTANCE_SQ_THRESHOLD && crate.linear_velocity.length_squared() < FAILURE_VELOCITY_SQ_THRESHOLD:
+	if distance_sq < FAILURE_DISTANCE_SQ_THRESHOLD:
 		crate.suicide()
-		print("singularity kill")
 
