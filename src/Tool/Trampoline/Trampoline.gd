6081175524000f989ba52fcb9446f91e2bbadbd9
Louis MOUHAT <louis.mouhat@gmail.com>
2023-05-01 16:47:57 +0200

    fix bumper physics

--- src/Tool/Trampoline/Trampoline.gd
+++ src/Tool/Trampoline/Trampoline.gd
@@ -1,22 +1,15 @@
 extends PhysicsTool
 
-const PUSH_FORCE = 200
+const PUSH_FORCE = 500
 
 @onready var animation_player = $"../AnimationPlayer"
-
-var forceVector: Vector2
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	forceVector = Vector2(0, -1)
-#	animation_player.play("RESET")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func add_physics_modifier(crate: Crate):
-	print('Force Applied')
+	var forceVector = Vector2.UP.rotated(self.rotation)
 	crate.apply_central_impulse(forceVector * PUSH_FORCE)
 	animation_player.stop()
 	animation_player.play("spring")
