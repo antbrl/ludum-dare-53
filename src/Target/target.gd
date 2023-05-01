3e48b7f4d9b9b61d47130e643303278eabf6acf0
Louis MOUHAT <louis.mouhat@gmail.com>
2023-05-01 23:37:14 +0200

    add new target

--- src/Target/target.gd
+++ src/Target/target.gd
@@ -1,6 +1,7 @@
-extends AnimatableBody2D
+extends Area2D
 signal crate_dropped(crate)
 
-func _on_target_body_entered(body):
+func _on_target_body_entered(body: RigidBody2D):
 	body.hit = true
+	body.linear_velocity = Vector2.ZERO;
 	emit_signal("crate_dropped", body)
