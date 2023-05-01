c63232140c3aeade61c1511df51347823d9d40b5
Antoine Brûlé <antoine.brule@netc.eu>
2023-05-01 15:34:30 +0200

    overhaul trampoline

--- src/Tool/Trampoline/Trampoline.gd
+++ src/Tool/Trampoline/Trampoline.gd
@@ -2,12 +2,14 @@ extends PhysicsTool
 
 const PUSH_FORCE = 200
 
+@onready var animation_player = $"../AnimationPlayer"
+
 var forceVector: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	forceVector = Vector2(0, -1)
-
+#	animation_player.play("RESET")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,4 +18,6 @@ func _process(delta):
 func add_physics_modifier(crate: Crate):
 	print('Force Applied')
 	crate.apply_central_impulse(forceVector * PUSH_FORCE)
+	animation_player.stop()
+	animation_player.play("spring")
 
