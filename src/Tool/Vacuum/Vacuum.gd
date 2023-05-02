97f8001914a77d1cf2ca8575d163eb96361b7f79
Louis MOUHAT <louis.mouhat@gmail.com>
2023-05-02 02:51:13 +0200

    "fix" vacuum

--- src/Tool/Vacuum/Vacuum.gd
+++ src/Tool/Vacuum/Vacuum.gd
@@ -6,9 +6,9 @@ var direction_vec: Vector2:
 	get:
 		return input.global_position.direction_to(output.global_position)
 
-const modif = 5e6
+const modif = 5e1
 const MAX_FORCE = Vector2(2e3, 2e3)
-const DETECTION_RADIUS = 20
+const DETECTION_RADIUS = 50
 @export var OUTPUT_VELOCITY = 600
 
 @export var direction: Globals.Direction = Globals.Direction.NONE:
@@ -33,7 +33,7 @@ func add_physics_modifier(crate: Crate):
 	var dir = input.global_position - crate.position
 	var distance = dir.length()
 	var entering = distance <= DETECTION_RADIUS
-	if entering:
+	if entering and crate.linear_velocity.dot(direction_vec) > 0:
 		crate.position = output.global_position + direction_vec * 8
 		crate.linear_velocity = direction_vec * OUTPUT_VELOCITY
 	else:
